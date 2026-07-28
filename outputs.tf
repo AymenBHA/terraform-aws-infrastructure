f436638f4a3e3344da04bf368d20b542fbc4afe2
AymenBHA <aymenboulilaa@gmail.com>
2026-07-28 23:08:01 +0000

    terraform outputs

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "instance_id" {
+  value = module.compute.instance_id
+}
+
+output "public_ip" {
+  value = module.compute.public_ip
+}
+
+output "public_dns" {
+  value = module.compute.public_dns
+}
